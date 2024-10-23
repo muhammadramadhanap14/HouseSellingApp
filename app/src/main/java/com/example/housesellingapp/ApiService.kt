23e4ac8975e87da5c17f49b15ed6d51edcab6404
M. Ramadhan Aditya P <114163931+muhammadramadhanap14@users.noreply.github.com>
2024-10-24 06:20:55 +0700

    Update ApiService.kt

--- app/src/main/java/com/example/housesellingapp/ApiService.kt
+++ app/src/main/java/com/example/housesellingapp/ApiService.kt
@@ -4,9 +4,9 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
-    @GET("listings.json") // Hapus URL lengkap dan hanya gunakan path relatif
+    @GET("listings.json") 
     suspend fun getSearchResults(): List<Listing>?
 
-    @GET("details/{id}.json") // Pastikan untuk menggunakan path yang benar
+    @GET("details/{id}.json") 
     suspend fun getListingDetail(@Path("id") id: Int): ListingDetail?
 }
