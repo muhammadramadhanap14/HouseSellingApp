430a09ac4e031f5828d669fb1bde890dafa34eab
M. Ramadhan Aditya P <114163931+muhammadramadhanap14@users.noreply.github.com>
2024-10-25 12:26:26 +0700

    Update Data.kt

--- app/src/main/java/com/example/housesellingapp/Data.kt
+++ app/src/main/java/com/example/housesellingapp/Data.kt
@@ -1,49 +1,56 @@
-package com.example.housesellingapp
+    package com.example.housesellingapp
 
-data class Listing(
-    val id: Int,
-    val project_name: String,
-    val address: Address,
-    val attributes: Attributes,
-    val photo: String
-)
+    data class Listing(
+        val id: Int,
+        val project_name: String,
+        val address: Address,
+        val attributes: Attributes,
+        val photo: String
+    )
 
-data class Address(
-    val district: String,
-    val street_name: String
-)
+    data class Address(
+        val district: String,
+        val street_name: String
+    )
 
-data class Attributes(
-    val area_size: Int,
-    val bathrooms: Int,
-    val bedrooms: Int,
-    val price: Int,
-    val pricePerSquareFoot: Double, 
-    val floorLevel: String 
-)
+    data class Attributes(
+        val area_size: Int,
+        val bathrooms: Int,
+        val bedrooms: Int,
+        val price: Int,
+        val pricePerSquareFoot: Double,
+        val floorLevel: String,
+        var furnishing: String,
+        val facing: String,
+        val overlooking_view: String,
+        val built_year: Int,
+        val tenure: String,
+        val property_type: String,
+        val last_update: String
+    )
 
-data class ListingDetail(
-    val id: Int,
-    val project_name: String,
-    val address: AddressDetail,
-    val attributes: Attributes,
-    val description: String,
-    val photo: String,
-    val property_details: List<PropertyDetail>
-)
+    data class ListingDetail(
+        val id: Int,
+        val project_name: String,
+        val address: AddressDetail,
+        val attributes: Attributes,
+        val description: String,
+        val photo: String,
+        val property_details: List<PropertyDetail>
+    )
 
-data class AddressDetail(
-    val title: String,
-    val subtitle: String,
-    val map_coordinates: MapCoordinates
-)
+    data class AddressDetail(
+        val title: String,
+        val subtitle: String,
+        val map_coordinates: MapCoordinates
+    )
 
-data class MapCoordinates(
-    val lat: Double,
-    val lng: Double
-)
+    data class MapCoordinates(
+        val lat: Double,
+        val lng: Double
+    )
 
-data class PropertyDetail(
-    val label: String,
-    val text: String
-)
+    data class PropertyDetail(
+        val label: String,
+        val text: String
+    )
